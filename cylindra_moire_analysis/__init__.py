1587297eddd3ff5b38e6b8d94dcd39f182b8b553
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2025-11-14 13:47:31 +0000

    add param

--- cylindra_moire_analysis/__init__.py
+++ cylindra_moire_analysis/__init__.py
@@ -1,4 +1,4 @@
-from .core import measure_skew, export_for_tubulej
+from cylindra_moire_analysis.core import export_for_tubulej, measure_skew
 
 __version__ = "0.1.0"
 __all__ = ["measure_skew", "export_for_tubulej"]
--- cylindra_moire_analysis/core.py
+++ cylindra_moire_analysis/core.py
@@ -1,22 +1,36 @@
-from typing import Annotated, NamedTuple
+from typing import Annotated
+
+import numpy as np
+from cylindra.annotations import BinSizeType, SplinesType
 from cylindra.plugin import register_function
 from cylindra.widgets import CylindraMainWidget
-from cylindra.annotations import SplinesType, BinSizeType
-from matplotlib import pyplot as plt
 from magicclass.types import Path
-import numpy as np
-from cylindra_moire_analysis.utils import find_min_near_center, filter_filament
+from matplotlib import pyplot as plt
+
+from cylindra_moire_analysis.utils import filter_filament, find_min_near_center
+
 
 @register_function
 def measure_skew(
     ui: CylindraMainWidget,
     splines: SplinesType,
     bin_size: BinSizeType = 1,
-    dx: Annotated[float, {"label": "inter-protofilament distance (nm)", "min": 2, "max": 6, "step": 0.001}] = 4.895,
     filament_width: Annotated[float, {"label": "filament width (nm)"}] = 30.0,
+    dx: Annotated[
+        float,
+        {
+            "label": "inter-protofilament distance (nm)",
+            "min": 2,
+            "max": 6,
+            "step": 0.001,
+        },
+    ] = 4.895,
+    min_moier_periodicity: Annotated[
+        float, {"label": "minimum moire periodicity (nm)"}
+    ] = 200.0,
 ):
     """Measure skew angle in the traditional way that uses moire pattern.
-    
+
     This method does essentially the same analysis as described in Ku et al., 2020,
     Bio-protocols, with some autmations.
 
@@ -26,36 +40,41 @@ def measure_skew(
         Spline indices to be analyzed.
     bin_size : int, default 1
         Binning factor.
-    dx : nm, default 4.895
-        Inter-protofilament distance.
     filament_width : nm, default 30.0
         Width of the straightened images. Must be larger than the diameter of the
         filament but small enough to exclude noise.
+    dx : nm, default 4.895
+        Inter-protofilament distance.
+    min_moier_periodicity : nm, default 200.0
+        Minimum moire periodicity to consider. Setting to larger value will restrict
+        the search range and may help to avoid false detection.
     """
-    img_st_list = ui.tomogram.straighten(
-        splines, binsize=bin_size, size=filament_width
-    )
+    img_st_list = ui.tomogram.straighten(splines, binsize=bin_size, size=filament_width)
     props: dict[int, dict] = {}
     ui.logger.print_html("<h3> --- Moire pattern analysis --- </h3>")
-    for i, img_st in zip(splines, img_st_list):
+    for i, img_st in zip(splines, img_st_list, strict=False):
         spl = ui.splines[i]
         skew_sign = np.sign(spl.props.get_glob("skew_angle", 1))
         ui.logger.print_html(f"<b>Spline-{i}</b>")
+        ui.logger.print(f"Length = {spl.length():.1f} nm")
         img_proj = img_st.mean(axis=0)
         img_filt, img_ft = filter_filament(img_proj)
-        ps_proj = np.fft.fftshift(img_ft.real ** 2 + img_ft.imag ** 2)
+        ps_sl = slice(0, int(0.55 * img_ft.shape[0]))
+        ps_proj = np.fft.fftshift(img_ft.real**2 + img_ft.imag**2)[ps_sl]
 
         with ui.logger.set_plt():
             plt.figure(figsize=(4, 4))
             plt.title("Power spectrum")
-            plt.imshow(ps_proj, cmap="inferno", vmin=0, vmax=np.percentile(ps_proj, 99.9))
+            plt.imshow(
+                ps_proj, cmap="inferno", vmin=0, vmax=np.percentile(ps_proj, 99.9)
+            )
             plt.axis("off")
             plt.tight_layout()
             plt.show()
-        
+
         with ui.logger.set_plt():
-            fig, axes = plt.subplots(nrows=2, figsize=(6, 3.2), sharex=True)
-            
+            _, axes = plt.subplots(nrows=2, figsize=(6, 3.2), sharex=True)
+
             # find the horizontal line that has the maximum amplitude in the real space
             plt.sca(axes[0])
             iyopt = np.argmax(np.std(img_filt, axis="x"))
@@ -63,27 +82,28 @@ def measure_skew(
             profile = img_filt[f"x={ix}"]
             plt.imshow(img_filt.T, cmap="gray")
             plt.axhline(ix + 0.5, color="lime", lw=1)
-            
+
             plt.sca(axes[1])
             plt.plot(profile, color="green")
             plt.xlabel("Position (px)")
             plt.tight_layout(h_pad=0.0)
             plt.show()
-            
+
             # power spectrum analysis
             plt.figure(figsize=(5, 2))
-            ymax_loc_ps = int(round(img_proj.shape.y * img_proj.scale.y / 200))
+            ymax_loc_ps = int(
+                round(img_proj.shape.y * img_proj.scale.y / min_moier_periodicity)
+            )
             upsample_factor = 50
             spec = (profile - profile.mean()).local_power_spectra(
-                f"y=0:{ymax_loc_ps}",
-                upsample_factor=upsample_factor
+                f"y=0:{ymax_loc_ps}", upsample_factor=upsample_factor
             )
             wnum = np.argmax(spec) / upsample_factor
             plt.title("Power spectrum of the line profile")
             plt.plot(np.arange(spec.size) / upsample_factor, spec, color="gray")
             plt.axvline(wnum, color="red", lw=1, ls="--")
             plt.show()
-        
+
         if wnum == 0:
             skew_moire = 0.0
         else:
@@ -91,7 +111,7 @@ def measure_skew(
             ui.logger.print(f"L = {dist:.1f} nm, δx = {dx:.3f} nm")
             skew_moire = np.rad2deg(np.arcsin(dx / dist))
         props[i] = {"moire_skew_angle": skew_moire * skew_sign}
-    
+
         for prop in props.values():
             spl.props.update_glob(prop, bin_size=bin_size)
 
@@ -101,6 +121,7 @@ def measure_skew(
         table.append(row)
     ui.logger.print_html("<b><u>Summary</u></b>")
     ui.logger.print_table(table, header=False)
+
 
 @register_function(name="Export for TubuleJ", record=False)
 def export_for_tubulej(
@@ -113,8 +134,8 @@ def export_for_tubulej(
     project_prefix: str = "MT_",
 ):
     """Export the straightened and filtered images.
-    
-    This method creates directories in a format compatible with the manual analysis 
+
+    This method creates directories in a format compatible with the manual analysis
     using TubuleJ.
 
     Parameters
@@ -135,10 +156,8 @@ def export_for_tubulej(
     """
     if not save_dir.exists():
         raise FileNotFoundError(f"Directory '{save_dir}' does not exist.")
-    img_st_list = ui.tomogram.straighten(
-        splines, binsize=bin_size, size=filament_width
-    )
-    for i, img_st in zip(splines, img_st_list):
+    img_st_list = ui.tomogram.straighten(splines, binsize=bin_size, size=filament_width)
+    for i, img_st in zip(splines, img_st_list, strict=False):
         mt_dir = save_dir / f"{project_prefix}{i}"
         mt_dir.mkdir(exist_ok=True)
         img_proj = img_st.mean(axis=0)
